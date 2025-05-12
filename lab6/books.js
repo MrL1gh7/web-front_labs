2514436ea27adc1e76605c2229f98eb72bfe376b
Light <ckfdf.slava00@gmail.com>
2025-05-12 22:08:13 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 500,
+        has: 1,
+        bestseller: 0
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 450,
+        has: 1,
+        bestseller: 1
+    },
+    {
+        author: 'Эрих Мария Ремарк',
+        name: 'Три товарища',
+        publicationYear: 1936,
+        price: 600,
+        has: 0,
+        bestseller: 0
+    },
+    {
+        author: 'Маргарет Митчелл',
+        name: 'Унесённые ветром',
+        publicationYear: 1936,
+        price: 550,
+        has: 0,
+        bestseller: 0
+    },
+    {
+        author: 'Антуан де Сент-Экзюпери',
+        name: 'Маленький принц',
+        publicationYear: 1943,
+        price: 400,
+        has: 0,
+        bestseller: 1
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 550,
+        has: 1,
+        bestseller: 1
+    },
+    {
+        author: 'Дж. К. Роулинг',
+        name: 'Гарри Поттер и философский камень',
+        publicationYear: 1997,
+        price: 650,
+        has: 1,
+        bestseller: 1
+    },
+    {
+        author: 'Патрик Ротфусс',
+        name: 'Имя ветра',
+        publicationYear: 2007,
+        price: 700,
+        has: 1,
+        bestseller: 0
+    },
+    {
+        author: 'Дуглас Адамс',
+        name: 'Автостопом по галактике',
+        publicationYear: 1979,
+        price: 500,
+        has: 1,
+        bestseller: 0
+    },
+    {
+        author: 'Пауло Коэльо',
+        name: 'Алхимик',
+        publicationYear: 1988,
+        price: 430,
+        has: 1,
+        bestseller: 0
+    },
+];
